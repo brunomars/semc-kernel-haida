88dbc746538fd1ed6ec2f8fe91f99aae6c6c472d
Ezekeel <notezekeel@googlemail.com>
2012-01-05 01:21:25 +0100

    Increased default readahead value to 1024kB.
    Conflicts:
    	include/linux/mm.h

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 39 */
+/* This file is auto generated, version 40 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#39 PREEMPT Mon Feb 4 01:27:28 EST 2013"
-#define LINUX_COMPILE_TIME "01:27:28"
+#define UTS_VERSION "#40 PREEMPT Mon Feb 4 01:36:59 EST 2013"
+#define LINUX_COMPILE_TIME "01:36:59"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
