70e74a4af47a7737cb3a126691515555e6fd4aec
adridu59 <adrien.tetar@hotmail.fr>
2012-05-26 15:48:53 +0200

    cpufreq: added VDD sysfs interface
    Conflicts:
    	drivers/cpufreq/Kconfig

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 40 */
+/* This file is auto generated, version 41 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#40 PREEMPT Mon Feb 4 01:36:59 EST 2013"
-#define LINUX_COMPILE_TIME "01:36:59"
+#define UTS_VERSION "#41 PREEMPT Mon Feb 4 01:42:57 EST 2013"
+#define LINUX_COMPILE_TIME "01:42:57"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
