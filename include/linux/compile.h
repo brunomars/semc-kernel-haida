31aae7baa33298c5354dcd19d4c2c2a394bb5058
adridu59 <adrien.tetar@hotmail.fr>
2012-05-27 21:20:34 +0200

    block: tweaked deadline fifo_batch
    Conflicts:
    	block/deadline-iosched.c

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 25 */
+/* This file is auto generated, version 33 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#25 PREEMPT Sun Feb 3 23:54:41 EST 2013"
-#define LINUX_COMPILE_TIME "23:54:41"
+#define UTS_VERSION "#33 PREEMPT Sun Feb 3 23:58:18 EST 2013"
+#define LINUX_COMPILE_TIME "23:58:18"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
