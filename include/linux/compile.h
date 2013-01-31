48b3edd1be7fa465205a70923b07c19fbf929db4
nicholaschw <nicholas.npcc@gmail.com>
2013-01-31 04:17:00 -0500

    Updates to sources

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 2 */
+/* This file is auto generated, version 3 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#2 PREEMPT Thu Jan 31 03:55:23 EST 2013"
-#define LINUX_COMPILE_TIME "03:55:23"
+#define UTS_VERSION "#3 PREEMPT Thu Jan 31 04:15:16 EST 2013"
+#define LINUX_COMPILE_TIME "04:15:16"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
