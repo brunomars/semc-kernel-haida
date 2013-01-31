1459204912676d73e7f09e8c53548ce8bdfcf20f
nicholaschw <nicholas.npcc@gmail.com>
2013-01-31 04:06:52 -0500

    Stash changes

---
+++ include/linux/compile.h
@@ -0,0 +1,9 @@
+/* This file is auto generated, version 2 */
+/*  PREEMPT */
+#define UTS_MACHINE "arm"
+#define UTS_VERSION "#2 PREEMPT Thu Jan 31 03:55:23 EST 2013"
+#define LINUX_COMPILE_TIME "03:55:23"
+#define LINUX_COMPILE_BY "salvador"
+#define LINUX_COMPILE_HOST "ubuntu"
+#define LINUX_COMPILE_DOMAIN "ubuntu-domain"
+#define LINUX_COMPILER "gcc version 4.4.3 (GCC) "
