2b7eeba5853a72c9fc5344408948be2235b9ad1f
brunomars <nicholas.npcc@gmail.com>
2013-02-03 22:31:16 -0500

    Push new modified .h files

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 4 */
+/* This file is auto generated, version 5 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#4 PREEMPT Thu Jan 31 04:24:23 EST 2013"
-#define LINUX_COMPILE_TIME "04:24:23"
+#define UTS_VERSION "#5 PREEMPT Thu Jan 31 05:02:39 EST 2013"
+#define LINUX_COMPILE_TIME "05:02:39"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
