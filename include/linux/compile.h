d0fa5bf61664a2d68c1294311aa3d36d1d7cb0e7
brunomars <nicholaschen35@gmail.com>
2013-02-04 02:07:04 -0500

    r57: Updates for Source

--- include/linux/compile.h
+++ include/linux/compile.h
@@ -1,8 +1,8 @@
-/* This file is auto generated, version 42 */
+/* This file is auto generated, version 43 */
 /*  PREEMPT */
 #define UTS_MACHINE "arm"
-#define UTS_VERSION "#42 PREEMPT Mon Feb 4 01:52:33 EST 2013"
-#define LINUX_COMPILE_TIME "01:52:33"
+#define UTS_VERSION "#43 PREEMPT Mon Feb 4 02:01:48 EST 2013"
+#define LINUX_COMPILE_TIME "02:01:48"
 #define LINUX_COMPILE_BY "salvador"
 #define LINUX_COMPILE_HOST "ubuntu"
 #define LINUX_COMPILE_DOMAIN "ubuntu-domain"
